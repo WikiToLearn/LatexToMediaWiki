284f9172b30525abeb2de538d04d710ea7a2d787
Davide Valsecchi <valsecchi.davide94@gmail.com>
2015-09-30 00:50:06 +0200

    Corrected test

--- test.py
+++ test.py
@@ -7,37 +7,37 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-#XML RENDERER
-f = open('tex_sources/DispenseStruttura.tex','r')
-text = f.read().decode('utf-8')
-tex2 = TeX()
-tex2.ownerDocument.config['files']['split-level'] = -100
-tex2.ownerDocument.config['files']['filename'] = 'DispenseStruttura.xml'
-tex2.input(text)
-document2 = tex2.parse()
-rend = XMLRenderer()
-rend.render(document2)
+# #XML RENDERER
+# f = open('tex_sources/DispenseStruttura.tex','r')
+# text = f.read().decode('utf-8')
+# tex2 = TeX()
+# tex2.ownerDocument.config['files']['split-level'] = -100
+# tex2.ownerDocument.config['files']['filename'] = 'DispenseStruttura.xml'
+# tex2.input(text)
+# document2 = tex2.parse()
+# rend = XMLRenderer()
+# rend.render(document2)
 
 #MEDIAWIKI RENDERER
 f2 = open('tex_sources/DispenseStruttura.tex','r')
 text2 = f2.read().decode('utf-8')
-tex3 = TeX()
-tex3.ownerDocument.config['files']['split-level'] = -100
+tex2 = TeX()
+tex2.ownerDocument.config['files']['split-level'] = -100
 tex2.ownerDocument.config['files']['filename'] = 'DispenseStruttura.mww'
-tex3.input(text2)
-document3 = tex3.parse()
-rend = MediaWikiRenderer("Test")
-rend.render(document3)
+tex2.input(text2)
+document2 = tex2.parse()
+rend2 = MediaWikiRenderer("Test")
+rend2.render(document2)
 #elaborazione
-rend.tree.collapseText(0)
-rend.tree.fixReferences()
-xml = rend.tree.exportXML()
+rend2.tree.collapseText(0)
+rend2.tree.fixReferences()
+xml = rend2.tree.exportXML()
 o = open('test.mw','w')
 o.write(xml)
 
 
 #DEBUg PRINTING
 #print(str(rend.tree.index))
-for k in rend.tree.pages:
-	print(str(rend.tree.pages[k]))
+for k in rend2.tree.pages:
+	print(str(rend2.tree.pages[k]))
 	#print(rend.tree.pages[k].text)
