13f118ac4b3d551759e84430a516715f0bcde21a
Davide Valsecchi <valsecchi.davide94@gmail.com>
2015-11-12 19:34:51 +0100

    Added method to remove commands without touching  nested brakets

--- l2mw/PreParser.py
+++ l2mw/PreParser.py
@@ -3,7 +3,7 @@ import re
 '''Function that searches \newtheorem command in tex source to find
 the theorems environments used. It memorizes them and it normalize them with 
 our theorem env'''
-def preparseTheorems(tex):
+def preparseTheorems(tex,print_path):
 	th_dict = {}
 	p = re.compile(ur'\\newtheorem\*?\{(.*?)\}(\[.*?\])?\{(.*?)\}')
 	for match in re.finditer(p,tex):
@@ -22,6 +22,34 @@ def preparseTheorems(tex):
 		#for text after theorem
 		new_tag_close = u'\\end{theorem}\n'
 		tex = tex.replace(tag_close, new_tag_close)
+
+	if(print_path != ""):
+		o = open(print_path,"w")
+		o.write(tex)
+		o.close()
 	return (tex, th_dict)
 
+
+def greedy_remove_command(tex,command):
+	result=''
+	tokens = tex.split(command)
+	#Remove the fist token that doesn't contain data and save it
+	result+= tokens[0]
+	tokens = tokens[1:]
+	#analyzing each token
+	for tok in tokens:
+		level = 0
+		pos = -1
+		for ch in tok:
+			pos+=1
+			if ch=='{':
+				level+=1
+			elif ch=='}':
+				level-=1
+			#now check if we are returned to 0 level
+			if level ==0:
+				#we can get che content of outer {}
+				result+=tok[1:pos] + ' '+ tok[pos+1:]
+				break;
+	return result;
 
